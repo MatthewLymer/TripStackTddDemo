ba4dc5ba45bda345958d5a0421e86963c57c98c4
Matthew Lymer <matthew.lymer@tripstack.com>
2020-03-26 10:04:57 -0400

    WIP: Base demo setup

---
+++ src/TripStack.TddDemo.CurrencyConverter.Abstractions/IConvertCurrencies.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TripStack.TddDemo.CurrencyConverter.Abstractions
+{
+    public interface IConvertCurrencies
+    {
+        Task<decimal> ConvertAsync(string fromCurrency, string toCurrency, CancellationToken token);
+    }
+}
---
+++ src/TripStack.TddDemo.CurrencyConverter.Abstractions/UnsupportedCurrencyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TripStack.TddDemo.CurrencyConverter.Abstractions
+{
+    public sealed class UnsupportedCurrencyException : Exception
+    {
+        public UnsupportedCurrencyException(string currencyCode)
+            : base($"The currency '{currencyCode}' is not supported.")
+        {
+        }
+    }
+}
