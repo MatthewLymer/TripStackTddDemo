8488083c03a0cd9ed3a8e311eb55bc9b745b08a5
Matthew Lymer <matthew.lymer@tripstack.com>
2020-03-30 16:37:42 -0400

    Setup unit test fixture

---
+++ tests/TripStack.TddDemo.StoreApi.Tests/CurrencyExchange/CachingGetExchangeRatesDecoratorTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace TripStack.TddDemo.StoreApi.Tests.CurrencyExchange
+{
+    public sealed class CachingGetExchangeRatesDecoratorTests
+    {
+        [Fact]
+        public void ShouldPass()
+        {
+            Assert.True(true);
+        }
+    }
+}
