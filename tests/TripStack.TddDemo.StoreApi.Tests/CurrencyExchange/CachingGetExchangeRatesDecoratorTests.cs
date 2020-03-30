46313a3aec76806bd21d7cca656f56675ae15e09
Matthew Lymer <matthew.lymer@tripstack.com>
2020-03-30 16:53:05 -0400

    Inherit from IGetExchangeRates

--- tests/TripStack.TddDemo.StoreApi.Tests/CurrencyExchange/CachingGetExchangeRatesDecoratorTests.cs
+++ tests/TripStack.TddDemo.StoreApi.Tests/CurrencyExchange/CachingGetExchangeRatesDecoratorTests.cs
@@ -1,17 +1,25 @@
-﻿using Xunit;
+﻿using System.Threading;
+using System.Threading.Tasks;
+using TripStack.TddDemo.CurrencyConverter.Abstractions;
+using Xunit;
 
 namespace TripStack.TddDemo.StoreApi.Tests.CurrencyExchange
 {
     public sealed class CachingGetExchangeRatesDecoratorTests
     {
         [Fact]
-        public void ShouldCreateInstance()
+        public void ShouldInheritFromIGetExchangeRates()
         {
-            var _ = new CachingGetExchangeRatesDecorator();
+            var decorator = new CachingGetExchangeRatesDecorator();
+            Assert.IsAssignableFrom<IGetExchangeRates>(decorator);
         }
     }
 
-    public sealed class CachingGetExchangeRatesDecorator
+    public sealed class CachingGetExchangeRatesDecorator : IGetExchangeRates
     {
+        public Task<decimal> GetExchangeRateAsync(string fromCurrency, string toCurrency, CancellationToken token)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
